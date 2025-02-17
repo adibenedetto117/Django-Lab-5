f84af95c6f9182bee6581068e911b6bbf705937d
adibenedetto117 <75105824+adibenedetto117@users.noreply.github.com>
2025-02-17 15:40:15 -0600

    add optional html page

--- myapp/urls.py
+++ myapp/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import hello_world
+from .views import hello_world, hello_world_page
 
 urlpatterns = [
-    path('', hello_world), 
+    path('', hello_world),
+    path('page/', hello_world_page) 
 ]
--- myapp/views.py
+++ myapp/views.py
@@ -5,3 +5,6 @@ from django.http import JsonResponse
 # Create your views here.
 def hello_world(request):
     return JsonResponse({"Message": "Hello World!"})
+
+def hello_world_page(request):
+    return render(request, 'myapp/helloworld.html')
